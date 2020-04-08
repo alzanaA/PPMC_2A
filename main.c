f4d01c491a777f5f9b32fdfc8168b358b1042ce5
andaruarham <63153409+andaruarham@users.noreply.github.com>
2020-04-08 20:54:53 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main ()
+ {
+    char file [20];
+    int n;
+    node* list = (node*)malloc(sizeof(node));
+    list = NULL;
+    FILE* f;
+    printf("Masukkan file : ");
+    gets(file);
+    printf("Masukkan nilai n: ");
+    scanf("%d", &n); 
+    f=fopen(file, "r");
+    char data [1000];
+ 
+   while(fgets(data,1000,f)!=NULL){
+        char* x =strtok(data," \t\n");
+        while(x!=NULL){
+            append(&list,x);
+            x=strtok(NULL," \t\n");
+        }
+    }
+    int lenList = countLen(list);
+    int lenArr = sqrt(lenList)+1;
+    string* word[lenArr];
+    for (int i =0;i<lenArr;i++){
+        word[i] = (string*)calloc(lenArr,sizeof(string));
+    }
+    for (int i=0;i<lenArr;i++){
+        for (int j=0;j<lenArr;j++){
+            strcpy(word[i][j].kata,"");
+        }
+    }
+    moveArray(list,word,lenArr);
+    free(list);
+    string* key[lenArr];
+    for (int i =0;i<lenArr;i++){
+        key[i] = (string*)calloc(lenArr,sizeof(string));
+    }
+    string* value[lenArr];
+     for (int i =0;i<lenArr;i++){
+         value[i] = (string*)calloc(lenArr,sizeof(string));
+     }
+    makeKey(word,key,lenArr,n);
+    makeValue(word,value,lenArr,n);
+     for (int i=0;i<lenArr;i++){
+        for (int j=0;j<lenArr;j++){
+            if (strcmp(key[i][j].kata,"")!=0){
+                printf("%s ---> %s\n",key[i][j].kata,value[i][j].kata);
+            }
+        }
+    }
+    fclose(f);
+    return 0;
+}
