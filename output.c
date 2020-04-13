c932707315aa61f6fe9cb3449d13e498a8ac3e43
Imam-f <mohamadimamfirdaus17@gmail.com>
2020-04-13 09:52:14 +0700

    Tambah 2 fungsi, paremeter output berubah

--- output.c
+++ output.c
@@ -2,31 +2,63 @@
 #include <output.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
-void search(char *c){
-    // is last
+int search(struct Queue * LUT, char * p)
+{
     // is found
-        // modify c
+    if(){
+        return 0;
+    } else if() {// is last
+        return 0;
+    } else {
+        return 1 + search();
+    }
 }
 
-void output(int n, int m){
+char* getKey(struct Queue * LUT, int index)
+{
+    return void;
+}
+
+char* getValue(struct Queue * LUT, int index)
+{
+    return void;
+}
+
+void output(int n, int m, struct Queue * LUT)
+{
+    int i;
     srand(time(0));
+    
     // buffer n kata
     char buffer[1000];
+    char value[1000];
 
     // ambil n kata random
-    int random_int = rand()*rand() % n;
-    
+    int word_index = rand()*rand() % n;
+    strcpy(buffer, getKey(LUT, word_index));
+
     // cetak n kata
-    print("%s ",buffer);
-    
+    printf(". . . %s",buffer);
+
     // loop
+    for(i = 0; i<m; i++){
         // cari key n
-        find(buffer);
+        word_index = search(LUT,buffer);
+
+        // ambil value
+        strcpy(value, getValue(LUT, word_index));
+
         // cetak n
+        printf(" %s",value);
+
         // update buffer
+        strcpy(buffer, value);
+        strcpy(value, getKey(LUT, word_index));
+        strcat(value, " ");
+        strcat(value, buffer);
+        strcpy(buffer, value);
+    }
 }
 
-int main(void){
-    return 0;
-}
--- output.h
+++ output.h
@@ -1,2 +1,4 @@
-void output(int n, int m);
-void search(char *c);
+void output(int n, int m, struct Queue * LUT);
+int search(struct Queue * LUT, char * p);
+char* getValue(struct Queue * LUT, int index);
+char* getKey(struct Queue * LUT, int index);
