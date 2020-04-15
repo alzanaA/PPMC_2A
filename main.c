d68f278cd73fe54e2f7a123a7f53992ddbe2b5e5
andaruarham <63153409+andaruarham@users.noreply.github.com>
2020-04-15 20:34:21 +0700

    tambah komen

--- main.c
+++ main.c
@@ -22,7 +22,7 @@
 
 int main ()
 {
-    while(true)
+    while(true) // looping selama belum di quit
 	{
 		printf("********************  N-GRAM PROGRAM  ********************");
         printf("\n");
@@ -33,34 +33,34 @@ int main ()
         printf("\n");
 		getchar();
 		
-		char file [20];
+		char file [20];			//inisiasi
 		int n;
 		int m;
 		node* list = (node*)malloc(sizeof(node));
 		list = NULL;
 		FILE* f;
-		printf("Masukkan file (ketik quit untuk keluar): ");
+		printf("Masukkan file (ketik quit untuk keluar): "); // Meminta input user untuk file txt
 		gets(file);
 		
-		if(strcmp(file, "quit")==0){
+		if(strcmp(file, "quit")==0){	// program akan keluar jika user menginput 'quit'
 			printf("\nThanks for having us");
 			break;
 		} else {
-			while(true) 
+			while(true) //looping pada input n dan m
 			{
 				printf("\nMasukkan nilai n untuk n gram (input negatif untuk berganti file/quit): ");
-				scanf("%d", &n);
+				scanf("%d", &n);	// meminta input user untuk jumlah n pada n-gram
 				if(n<0){
-					break;
+					break; // akan keluar dari loop (kembali ke input file ext) jika memasukkan n < 0
 				}
 
-				printf("Masukkan jumlah kata yang ingin dicetak: ");
-				scanf("%d", &m);
+				printf("Masukkan jumlah kata yang ingin dicetak: "); 
+				scanf("%d", &m);	 // meminta input user untuk jumlah kata yang ingin ditampilkan pada output
 				f=fopen(file, "r");
 				char data [1000];
 			 
 				while(fgets(data,1000,f)!=NULL)
-				{
+				{						// parsing file
 					char* x =strtok(data," \t\n");
 					while(x!=NULL)
 					{
@@ -82,7 +82,7 @@ int main ()
 					}
 				}
 				
-				moveArray(list,word,lenArr);
+				moveArray(list,word,lenArr);	// memanggil fungsi movearray
 				free(list);
 				string* key[lenArr];
 				for (int i =0;i<lenArr;i++){
@@ -97,14 +97,14 @@ int main ()
 					}
 				}
 				
-				makeKey(word,key,lenArr,n,lenList);
-				makeValue(word,value,lenArr,n,lenList);
+				makeKey(word,key,lenArr,n,lenList);	// memanggil fungsi makeKey
+				makeValue(word,value,lenArr,n,lenList);	// memanggil fungsi makeValue
 			 
 				struct Queue* LUT= new_queue();
-				makeTable(LUT, key, value, lenArr);
+				makeTable(LUT, key, value, lenArr);	// memanggil fungsi makeTable (meletakkan key dan value pada table)
 			
                 printf("\n");
-				output(n,m,LUT);
+				output(n,m,LUT);	// memanggil fungsi output
                 printf("\n");
 				
 				fclose(f);
