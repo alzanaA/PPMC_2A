725d08ed7b2714e8c229f75e68cb96c767f19837
Imam-f <mohamadimamfirdaus17@gmail.com>
2020-04-15 17:46:59 +0700

    Rev_1

--- output.h
+++ output.h
@@ -3,8 +3,3 @@ int search(struct table * LUT, char * p, int m);
 char* getValue(struct table * LUT, int index, int m);
 char* getKey(struct table * LUT, int index);
 node * getValueRand(node * value, node * current, int word_index);
-
-
-typedef struct node;
-typedef struct table;
-struct Queue;
